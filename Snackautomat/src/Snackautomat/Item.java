9d631f7d3c709cf3b38b70034ce3cd4745498d5f
AnikSta <145564994+AnikSta@users.noreply.github.com>
2023-12-01 10:22:54 +0100

    added getter and setter to the items and money function

--- Snackautomat/src/Snackautomat/Item.java
+++ Snackautomat/src/Snackautomat/Item.java
@@ -1,4 +1,44 @@
 package Snackautomat;
 
 public class Item {
+
+    private float price;
+    private String nameOfTheProduct;
+    private final int productnumber;    //These are the fix product numbers
+    private int amout;
+
+    public Item(float price, String nameOfTheProduct, int productnumber, int amout) {
+        this.price = price;
+        this.nameOfTheProduct = nameOfTheProduct;
+        this.productnumber = productnumber;
+        this.amout = amout;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getNameOfTheProduct() {
+        return nameOfTheProduct;
+    }
+
+    public void setNameOfTheProduct(String nameOfTheProduct) {
+        this.nameOfTheProduct = nameOfTheProduct;
+    }
+
+    public int getProductnumber() {
+        return productnumber;
+    }
+
+    public int getAmout() {
+        return amout;
+    }
+
+    public void setAmout(int amout) {
+        this.amout = amout;
+    }
 }
--- Snackautomat/src/Snackautomat/Money.java
+++ Snackautomat/src/Snackautomat/Money.java
@@ -1,4 +1,49 @@
 package Snackautomat;
 
 public class Money {
+
+    private float price;
+    private String nameOfTheProduct;
+
+    private String productnumber;
+    private int amount;
+
+    public Money(float price, String nameOfTheProduct, String productnumber, int amount) {
+        this.price = price;
+        this.nameOfTheProduct = nameOfTheProduct;
+        this.productnumber = productnumber;
+        this.amount = amount;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getNameOfTheProduct() {
+        return nameOfTheProduct;
+    }
+
+    public void setNameOfTheProduct(String nameOfTheProduct) {
+        this.nameOfTheProduct = nameOfTheProduct;
+    }
+
+    public String getProductnumber() {
+        return productnumber;
+    }
+
+    public void setProductnumber(String productnumber) {
+        this.productnumber = productnumber;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
